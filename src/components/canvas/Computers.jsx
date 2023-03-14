876aaa3f563a40af07cca660a9dd37beaa862575
AntoineQuarroz <antoinequarroz.mediam@gmail.com>
2023-03-14 11:46:41 +0100

    Mise en place de l'ordinateur en Hero

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,15 +5,18 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
+const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
       <mesh>
         <hemisphereLight intensity={0.15} groundColor='black' />
-        <pointLight intensity={1} />
+          <pointLight intensity={1} />
         <primitive
             object={computer.scene}
+            scale={0.75}
+            position={[0, -3.25, -1.5]}
+            rotation={[-0.01, -0,2, -0,1]}
         />
       </mesh>
   );
