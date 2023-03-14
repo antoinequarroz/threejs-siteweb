11b438fec2e7088c867ce742a0aa94f1ab953caf
AntoineQuarroz <antoinequarroz.mediam@gmail.com>
2023-03-14 11:14:35 +0100

    Ajout du Computers component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
           <div>
             <h1 className={`${styles.heroHeadText} text-white`}>
-              Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+              Hello, je suis <span className='text-[#915EFF]'>Antoine</span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop 3D visuals, user <br className='sm:block hidden' />
-              interfaces and web applications
+             Médiamaticien spécialisé <br className='sm:block hidden' />
+              en développement web et applications mobiles.
             </p>
           </div>
         </div>
